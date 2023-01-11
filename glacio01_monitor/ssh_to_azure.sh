62d9a16485a8532c11c48238877d25283822fbf6
azure-aws <aws@aws>
2023-01-11 14:25:28 +0000

    bash script to be run on glacio01 cron

---
+++ glacio01_monitor/ssh_to_azure.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# ssh to the Azure aws server (using ssh config), then
+# rename the glacio01_* file with current datetime.
+# touch the file to reset the update time for the file
+
+# The file at Azure can then be monitored for update time
+# as a way to check that glacio01 is alive and well!
+
+# Patrick Wright, GEUS
+# Jan 11, 2023
+
+filename="glacio01_$(date -u +"%Y%m%dT%H%M").txt"
+ssh azure-aws -T "cd ~/monitoring/glacio01_monitor; mv glacio01_*.txt $filename; touch $filename"
