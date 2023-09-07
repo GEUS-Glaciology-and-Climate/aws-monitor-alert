b41914ac691d49166de29f2eac73bb7729873a79
Mads Christian Lund <maclu@geus.dk>
2023-09-07 10:23:27 +0200

    Fixed command line inputes to alert_processing_wrapper.sh

--- aws_processing_monitor/alert_processing_wrapper.sh
+++ aws_processing_monitor/alert_processing_wrapper.sh
@@ -5,8 +5,7 @@ conda activate py39-monitor
 echo "Running alert_processing.py at `date -u`"
 
 base_config=$(dirname $0)/aws_azure.ini
-python -m alert_processing.check_alerts \
-  -c $base_config \
-  -c /home/aws/aws-monitor-alert/credentials/credentials.ini
+python -m alert_processing.check_alerts -c $base_config /home/aws/aws-monitor-alert/credentials/credentials.ini
 
 echo "FINISHED"
+
